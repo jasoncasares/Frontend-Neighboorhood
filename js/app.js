fc57340601c07ecd1b744c77bd9786d8848e0554
Jason Casares <jasonc@csdrafting.com>
2016-02-26 14:08:52 -0800

    docs:First commit

---
+++ js/app.js
@@ -0,0 +1,127 @@
+//List of locations that binds to search
+var locationData = [
+  {
+    locationName: 'Intelligentisa Coffee',
+    latLng: {lat: 33.990981, lng: -118.466844},
+  }, 
+  {
+    locationName: 'Blue Bottle Coffee',
+    latLng: {lat: 33.991963, lng: -118.470312},
+  }, 
+  {
+    locationName: 'Lemonade',
+    latLng: {lat: 33.989347, lng: -118.462450},
+  }, 
+  {
+    locationName: 'Gjelina Take Away',
+    latLng: {lat: 33.990590, lng: -118.464999},
+  }, 
+  { 
+    locationName: 'The Tasting Kitchen',
+    latLng: {lat: 33.989701, lng: -118.463117}   
+  }
+];
+
+//Google Maps
+var initMap = function() {
+  var self = this;
+
+  //Google Map object
+	self.googleMap = new google.maps.Map(document.getElementById('map'), {
+  	center: {lat: 33.991, lng: -118.467},
+  	zoom: 17 
+  });
+
+  //Build place objects
+  self.allPlaces = [];
+  locationData.forEach(function(place) {
+    self.allPlaces.push(new Place(place));
+  });
+
+  // Build markers
+  self.allPlaces.forEach(function(place) {
+    var markerOptions = {
+      map: self.googleMap,
+      position: place.latLng,
+      animation: google.maps.Animation.DROP
+    };
+    
+    // Create Marker
+    place.marker = new google.maps.Marker(markerOptions);
+
+    //Create info window
+    var contentString;
+    self.infowindow = new google.maps.InfoWindow({
+      content: contentString
+    });
+
+    //place.marker.addListener('click', function() {
+      //self.infowindow.open(map,marker)
+    //});
+
+    //Bounce animation when clicked
+    //marker.addListener('click', toggleBounce);
+    //});
+
+    //function toggleBounce() {
+      //if (marker.getAnimation() !==null) {
+        //marker.setAnimation(nutll);
+      //} else {
+        //marker.setAnimation(google.maps.Animation.BOUNCE);
+      //}
+    //};
+
+  });
+
+  //Array KO
+  self.visiblePlaces = ko.observableArray();
+  
+  
+ //View Places
+  self.allPlaces.forEach(function(place) {
+    self.visiblePlaces.push(place);
+  });
+
+  //Store User Input
+  self.userInput = ko.observable('');
+  
+  
+  // filter markers based on user input to see if it matches list
+  self.filterMarkers = function() {
+    var searchInput = self.userInput().toLowerCase();
+    
+    self.visiblePlaces.removeAll();
+    
+    // Name of place then determine if visible by user input
+    self.allPlaces.forEach(function(place) {
+      place.marker.setVisible(false);
+      
+      if (place.locationName.toLowerCase().indexOf(searchInput) !== -1) {
+        self.visiblePlaces.push(place);
+      }
+    });
+    
+    
+    self.visiblePlaces().forEach(function(place) {
+      place.marker.setVisible(true);
+    });
+  };
+  
+  
+  function Place(dataObj) {
+    this.locationName = dataObj.locationName;
+    this.latLng = dataObj.latLng;
+    
+    // You will save a reference to the Places' map marker after you build the
+    // marker:
+    this.marker = null;
+  }
+  
+};
+
+ko.applyBindings(new initMap());
+
+//Google Maps error when maps is down
+//function googleError() {
+  //alert("google API unavailable");
+//}
